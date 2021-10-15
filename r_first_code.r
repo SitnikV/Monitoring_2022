afa16a55aac07181ddb7730526399cb8a16f9418
Viktoria Sitnik <92572605+SitnikV@users.noreply.github.com>
2021-10-15 11:00:01 +0200

    Create r_first_code.r

---
+++ r_first_code.r
@@ -0,0 +1,24 @@
+# Excited to write my first code on Github
+
+# Constanza input data on streams
+ 
+stream<-c(100,120,300,400,100)
+
+# New set, Marta data on fishes genomes
+
+fishes<-c(10, 50, 60, 100, 200)
+ 
+
+
+# first plot in R, plotting correlation stream against fishes diversity
+
+plot(stream, fishes)
+
+# change colour on data points to green
+
+plot(stream, fishes, col=("green"))
+
+# change the shape of data points to squares
+
+plot(stream, fishes, col=("green"), pch=15)
+
