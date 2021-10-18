378abbbafb2dc892ecbfd102c686df0eab54ec98
Viktoria Sitnik <92572605+SitnikV@users.noreply.github.com>
2021-10-18 10:56:16 +0200

    Update r_first_code.r

--- r_first_code.r
+++ r_first_code.r
@@ -1,10 +1,10 @@
-# Excited to write my first code on Github
+# excited to write my first code on Github
 
 # Constanza input data on streams
  
 stream<-c(100,120,300,400,100)
 
-# New set, Marta data on fishes genomes
+# new set, Marta data on fishes genomes
 
 fishes<-c(10, 50, 60, 100, 200)
  
@@ -21,4 +21,20 @@ plot(stream, fishes, col=("green"))
 # change the shape of data points to squares
 
 plot(stream, fishes, col=("green"), pch=15)
+
+# the data we developed can be stored in a table
+# a table in R is called data frame
+
+data.frame(stream, fishes)
+
+# assign created table a name (water)
+
+water <-data.frame(stream, fishes)
+
+# water is a data frame now
+# to see the table use View function
+
+View(water)
+
+
 
