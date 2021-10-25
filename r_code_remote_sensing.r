25dc3d29bc44032fe01663f148f0945520dc3785
Viktoria Sitnik <92572605+SitnikV@users.noreply.github.com>
2021-10-25 11:03:26 +0200

    Create r_code_remote_sensing.r

---
+++ r_code_remote_sensing.r
@@ -0,0 +1,16 @@
+# R code for ecosystem monitoring by remote sensing
+# install raster package
+# https://cran.r-project.org/web/packages/raster/index.html
+
+install.packages("raster")
+
+# set wd
+
+setwd("/Users/viktoriasitnik/Documents/R/lab")
+
+library(raster)
+#No quotes needed as package is already in R
+
+# install package terra
+ install.packages("terra")
+
